465a0e96415d0972ccd486a19bf3ec5d0c4ed2c4
Dimitar Gyurov <dgyurov@gmail.com>
2016-04-25 19:37:35 +0200

    Added aspect ratio const. for all the Compose buttons

---
+++ Hikebeat/ModalVC.swift
@@ -0,0 +1,40 @@
+//
+//  ModalVC.swift
+//  Hikebeat
+//
+//  Created by Dimitar Gyurov on 4/25/16.
+//  Copyright © 2016 Niklas Gundlev. All rights reserved.
+//
+
+import UIKit
+
+class ModalVC: UIViewController {
+
+    @IBOutlet weak var infoContainer: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        infoContainer.layer.cornerRadius = infoContainer.bounds.height/4
+        infoContainer.layer.masksToBounds = true
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
