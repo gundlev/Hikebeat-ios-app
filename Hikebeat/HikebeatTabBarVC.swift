3987fb3aaf04f04a472fc94cd64f1c0d31d580d1
Niklas Stokkebro Gundlev <niklasgundlev@gmail.com>
2016-04-25 13:41:51 +0200

    Setup tabbar, still needs minor setup but functioning

--- Hikebeat/HikebeatTabBarVC.swift
+++ Hikebeat/HikebeatTabBarVC.swift
@@ -11,40 +11,61 @@ import UIKit
 
 class HikebeatTabBarVC: UITabBarController {
     
+    let centerButton = UIButton(type: UIButtonType.Custom)
+    
     override func viewDidLoad() {
         addCenterButton()
-        self.tabBar.tintColor = UIColor(red:0.082, green:0.404, blue:0.424, alpha:1.00)
-        self.tabBar.backgroundColor = UIColor(red:0.082, green:0.404, blue:0.424, alpha:1.00)
-        self.view.tintColor = UIColor(red:0.082, green:0.404, blue:0.424, alpha:1.00)
-        self.view.backgroundColor = UIColor(red:0.082, green:0.404, blue:0.424, alpha:1.00)
+        self.selectedIndex = 1
+        self.tabBar.barTintColor = UIColor(red:0.082, green:0.404, blue:0.424, alpha:1.00)
+        self.tabBar.tintColor = UIColor(red:176/255.0, green:250/255.0, blue:0, alpha:1.00)
         self.tabBar.translucent = false
-        
-        //self.view.tintColor = UIColor(red:0.082, green:0.404, blue:0.424, alpha:1.00)
+
     }
     
     func addCenterButton() {
 
-        let centerButton = UIButton(type: UIButtonType.Custom)
         let selected = UIImage(named: "TabBarMainSelected")!
         let unselected = UIImage(named: "TabBarMainUnselected")!
         centerButton.frame = CGRectMake(0.0, 0.0, selected.size.width, selected.size.height)
-        centerButton.setBackgroundImage(selected, forState: UIControlState.Selected)
-        centerButton.setBackgroundImage(selected, forState: UIControlState.Highlighted)
-        centerButton.setBackgroundImage(unselected, forState: UIControlState.Normal)
-        centerButton.selected = true
         
-        var heightDifference = self.tabBar.frame.size.height/2;
-//        if (heightDifference < 0) {
-//            centerButton.center = self.tabBar.center;
-//        } else {
-            var center:CGPoint = self.tabBar.center;
-            center.y = center.y - heightDifference;
-            centerButton.center = center;
-//        }
+        /* 
+         Purposefully setting UIControlState reversed to avoid having the button return to normal when holding a pres.
+         This should be fixed at some point but works perfectly right now. This means that the state "Selected" is true when
+         it is not selected and visa versa.
+        */
+        centerButton.setBackgroundImage(selected, forState: UIControlState.Normal)
+        centerButton.setBackgroundImage(unselected, forState: UIControlState.Selected)
+        centerButton.adjustsImageWhenHighlighted = false
+        centerButton.selected = false
+        centerButton.addTarget(self, action: #selector(HikebeatTabBarVC.centerButtonPressed), forControlEvents: UIControlEvents.TouchDown)
+        
+        let heightDifference = self.tabBar.frame.size.height/2;
+
+        var center:CGPoint = self.tabBar.center;
+        center.y = center.y - heightDifference;
+        centerButton.center = center;
         
         self.view.addSubview(centerButton)
 
 //        centerButton.autoresizingMask = UIViewAutoresizing.FlexibleRightMargin | UIViewAutoresizing.FlexibleLeftMargin | UIViewAutoresizing.FlexibleBottomMargin | UIViewAutoresizing.FlexibleTopMargin;
         
+    }
+    
+    func centerButtonPressed() {
+        print("button pressed")
+        self.selectedIndex = 1
+        // Reverse, the button is actually pressed here.
+        centerButton.selected = false
+    }
+    
+    override func tabBar(tabBar: UITabBar, didSelectItem item: UITabBarItem) {
+        
+        if item.tag != 1 {
+            // Reverse, the button is actually not pressed here.
+            centerButton.selected = true
+        } else {
+            // Reverse, the button is actually pressed here.
+            centerButton.selected = false
+        }
     }
 }
