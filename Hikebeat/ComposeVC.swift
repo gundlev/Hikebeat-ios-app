3257739d695f3f2b1b246e7e4d442190ec2a290a
Niklas Stokkebro Gundlev <niklasgundlev@gmail.com>
2016-04-30 14:56:42 +0200

    started working on sendBeat

--- Hikebeat/ComposeVC.swift
+++ Hikebeat/ComposeVC.swift
@@ -141,6 +141,219 @@ class ComposeVC: UIViewController {
         }
     }
     
+    /*
+     Sending beat functions
+     */
+    
+    func checkForCorrectInput() {
+        let locationTuple = self.getTimeAndLocation()
+        print(0.1)
+        if locationTuple != nil {
+            print(0.2)
+            if ((titleTextField.text == "" && messageTextView.text == "" && currentImage == nil && currentVideo == nil) || self.activeJourney == nil || locationTuple!.latitude == "" || locationTuple!.longitude == "" || locationTuple!.altitude == "") {
+                print(0.3)
+                // Give a warning that there is not text or no active journey.
+                print("Something is missing")
+                print("Text: ", titleTextField.text == "" && messageTextView.text == "" && currentImage == nil && currentVideo == nil)
+                print("Journey: ", self.activeJourney == nil)
+                print("Lat: ", locationTuple!.latitude)
+                print("Lng: ", locationTuple!.longitude)
+                
+            } else {
+                
+                print(0.4)
+                var title: String? = nil
+                var message: String? = nil
+                var mediaData: String? = nil
+                var orientation: String? = nil
+                var mediaType: String? = nil
+                print(0.5)
+                if titleTextField.text != "" || titleTextField.text != " " || titleTextField.text != "  " || titleText != "   " {
+                    title = self.titleTextField.text
+                }
+                print(0.6)
+                if messageTextView.text != "" || titleTextField.text != " " || titleTextField.text != "  " || titleTextField.text != "   "{
+                    message = self.messageTextView.text
+                }
+                print(0.7)
+                if currentImage != nil {
+                    //print(1)
+                    let imageData = UIImageJPEGRepresentation(currentImage!, 0.5)
+                    mediaType = MediaType.image
+                    //print(2)
+                    mediaData = saveMediaToDocs(imageData!, journeyId: (activeJourney?.journeyId)!, timestamp: locationTuple!.timestamp, fileType: ".jpg")
+
+                } else if currentMediaURL != nil {
+                    mediaType = MediaType.video
+                    let newPath = getPathToFileFromName("vid-temp.mp4")
+                    let success = covertToMedia(currentMediaURL!, pathToOuputFile: newPath!, fileType: AVFileTypeMPEG4)
+                    if success {
+                        let videoData = NSData(contentsOfURL: currentMediaURL!)
+                        mediaData = saveMediaToDocs(videoData!, journeyId: (activeJourney?.journeyId)!, timestamp: locationTuple!.timestamp, fileType: ".mp4")
+                        if mediaData != nil {
+                            self.removeMediaWithURL(currentMediaURL!)
+                        }
+                        print("mediaData: ", mediaData)
+                    }
+                    
+                } else if audioHasBeenRecordedForThisBeat {
+                    mediaType = MediaType.audio
+                    let pathToAudio = getPathToFileFromName("audio-temp.acc")
+                    let newPath = getPathToFileFromName("audio-temp.m4a")
+                    covertToMedia(pathToAudio!, pathToOuputFile: newPath!, fileType: AVFileTypeAppleM4A)
+                    let audioData = NSData(contentsOfURL: newPath!)
+                    mediaData = saveMediaToDocs(audioData!, journeyId: (activeJourney?.journeyId)!, timestamp: locationTuple!.timestamp, fileType: "m4a")
+                    self.recorder.deleteRecording()
+                }
+                
+                
+                
+                
+                //            let locationTuple = self.getTimeAndLocation()
+                print("Just Before Crash!")
+                self.currentBeat = DataBeat(context: (self.stack?.mainContext)!, title: title, journeyId: activeJourney!.journeyId, message: message, latitude: locationTuple!.latitude, longitude: locationTuple!.longitude, altitude: locationTuple!.altitude, timestamp: locationTuple!.timestamp, mediaType: mediaType, mediaData: mediaData, mediaDataId: nil, messageId: nil, mediaUploaded: false, messageUploaded: false, orientation:  orientation, journey: activeJourney!)
+                print("Just After Crash!")
+                self.sendBeat()
+            }
+        } else {
+            
+        }
+    }
+    
+    func sendBeat() {
+        
+        if ((titleTextField.text!.characters.count + messageTextView.text.characters.count) > 0) {
+            
+            // Check if there is any network connection and send via the appropriate means.
+            if SimpleReachability.isConnectedToNetwork() {
+                // TODO: send via alamofire
+                let url = IPAddress + "journeys/" + (activeJourney?.journeyId)! + "/messages"
+                print("url: ", url)
+                
+                var localTitle = ""
+                var localMessage = ""
+                if currentBeat!.message != nil {
+                    localMessage = currentBeat!.message!
+                }
+                if currentBeat!.title != nil {
+                    localTitle = currentBeat!.title!
+                }
+                // "headline": localTitle, "text": localMessage,
+                var parameters = ["lat": currentBeat!.latitude, "lng": currentBeat!.longitude, "alt": currentBeat!.altitude, "timeCapture": currentBeat!.timestamp]
+                if localTitle != "" {
+                    parameters["headline"] = localTitle
+                }
+                if localMessage != "" {
+                    parameters["text"] = localMessage
+                }
+                // Sending the beat message
+                Alamofire.request(.POST, url, parameters: parameters, encoding: .JSON, headers: Headers).responseJSON { response in
+                    print("The Response")
+                    print(response.response?.statusCode)
+                    print(response)
+                    
+                    // if response is 200 OK from server go on.
+                    if response.response?.statusCode == 200 {
+                        print("The text was send")
+                        self.currentBeat?.messageUploaded = true
+                        
+                        // Save the messageId to the currentBeat
+                        let rawMessageJson = JSON(response.result.value!)
+                        let messageJson = rawMessageJson["data"][0]
+                        self.currentBeat?.messageId = messageJson["_id"].stringValue
+                        
+                        // If the is an image in the currentBeat, send the image.
+                        if self.currentBeat?.mediaData != nil {
+                            print("There is an image or video")
+                            // Send Image
+                            
+                            let filePath = self.getPathToFileFromName((self.currentBeat?.mediaData)!)
+                            if filePath != nil {
+                                let urlMedia = IPAddress + "journeys/" + (self.activeJourney?.journeyId)! + "/media"
+                                print(urlMedia)
+                                
+                                var customHeader = Headers
+                                
+                                customHeader["x-hikebeat-timecapture"] = self.currentBeat?.timestamp
+                                customHeader["x-hikebeat-type"] = self.currentBeat?.mediaType!
+                                
+                                Alamofire.upload(.POST, urlMedia,headers: customHeader, file: filePath!).responseJSON { mediaResponse in
+                                    print("This is the media response: ", mediaResponse)
+                                    
+                                    // If everything is 200 OK from server save the imageId in currentBeat variable mediaDataId.
+                                    if mediaResponse.response?.statusCode == 200 {
+                                        let rawImageJson = JSON(mediaResponse.result.value!)
+                                        let mediaJson = rawImageJson["data"][0]
+                                        print(mediaResponse)
+                                        print("The image has been posted")
+                                        
+                                        // Set the imageId in currentBeat
+                                        print("messageId: ", mediaJson["_id"].stringValue)
+                                        self.currentBeat?.mediaDataId = mediaJson["_id"].stringValue
+                                        
+                                        // Set the uploaded variable to true as the image has been uplaoded.
+                                        self.currentBeat?.mediaUploaded = true
+                                        saveContext(self.stack.mainContext)
+                                    } else {
+                                        print("Error posting the image")
+                                        self.currentBeat?.mediaUploaded = false
+                                        saveContext(self.stack.mainContext)
+                                    }
+                                    
+                                    self.setInitial(true)
+                                    self.swipeView.setBack(true)
+                                    
+                                }
+                            }
+                        } else {
+                            print("There's no image")
+                            self.currentBeat?.mediaUploaded = true
+                            saveContext(self.stack.mainContext)
+                            self.setInitial(true)
+                            self.swipeView.setBack(true)
+                        }
+                        
+                        //Likely not usefull call to saveContext -> Test it!!
+                        saveContext(self.stack.mainContext)
+                    } else {
+                        // Error occured
+                        print("Error posting the message")
+                        alert("Problem sending", alertMessage: "Some error has occured when trying to send, it will be saved and syncronized later", vc: self, actions:
+                            (title: "Ok",
+                                style: UIAlertActionStyle.Cancel,
+                                function: {}))
+                        
+                        
+                        // Is set to true now but should be changed to false
+                        self.currentBeat?.mediaUploaded = false
+                        self.currentBeat?.messageUploaded = false
+                        saveContext(self.stack.mainContext)
+                    }
+                    
+                    // print(response)
+                    // if the response is okay run:
+                    // TODO: save the Beat
+                    saveContext(self.stack.mainContext)
+                    //                    self.saveCurrentBeat(uploaded)
+                    //self.setInitial(true)
+                }
+                //                self.setInitial(true)
+                //                self.swipeView.setBack(true)
+            } else {
+                
+                // This will send it via SMS.
+                print("Not reachable, should send sms")
+                let messageText = self.genSMSMessageString(titleTextField.text!, message: messageTextView.text, journeyId: self.activeJourney!.journeyId)
+                self.sendSMS(messageText)
+                // The save and setInitial is done in the message methods as it knows whether it fails.
+            }
+            
+            // TODO: save
+            
+        } else {
+            //TODO: Set alert to tell user that there's no text.
+        }
+    }
 
     
 /*
