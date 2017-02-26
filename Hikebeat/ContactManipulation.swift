dec1f3b31a025dc3b8eab58d56ae3b61bb39be49
Niklas Stokkebro Gundlev <niklasgundlev@gmail.com>
2017-02-26 21:21:42 +0100

    rewrote changes

---
+++ Hikebeat/ContactManipulation.swift
@@ -0,0 +1,9 @@
+//
+//  ContactManipulation.swift
+//  Hikebeat
+//
+//  Created by Niklas Gundlev on 23/02/2017.
+//  Copyright © 2017 Niklas Gundlev. All rights reserved.
+//
+
+import Foundation
---
+++ Hikebeat/Pair.swift
@@ -0,0 +1,9 @@
+//
+//  Pair.swift
+//  Hikebeat
+//
+//  Created by Niklas Gundlev on 23/02/2017.
+//  Copyright © 2017 Niklas Gundlev. All rights reserved.
+//
+
+import Foundation
